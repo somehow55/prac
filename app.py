489ef86a2ca8060f6fc60214442eff81c785008d
somehow55 <somehow55@gmail.com>
2022-01-02 00:54:47 +0900

    2022-01-02, flask API 만들기
    벌써 2022년이라니 시간 참 빠르다
    벌써 2022년이라니 시간 참 빠르다

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is Home!'\
+    # return 'This is Home!'
+    # return '<button>버튼데스</button>'
+    return render_template('index.html')
+
+
+@app.route('/test', methods=['GET'])
+def test_get():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+#  같은 창구(/test)지만 get을 받으면 위로, post를 받으면 아래로 간다
+
+@app.route('/test', methods=['POST'])
+def test_post():
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
 
 
 @app.route('/mypage')
